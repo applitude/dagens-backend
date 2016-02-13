907426abbba48a402af11b7659b9f3cc1be3e8b9
SirSJR <robinsaleh@hotmail.com>
2016-02-13 08:59:28 +0100

    Implementing a lambda request class for getting data from SIOs API. Added the AWS Lambda Java Events dependency to maven.

---
+++ src/main/java/no/applitude/dagensbackend/CafeteriaEndpoint.java
@@ -0,0 +1,36 @@
+package no.applitude.dagensbackend;
+
+public enum CafeteriaEndpoint {
+    ANDREA("285"),
+    ANNAS("281"),
+    ARKITEKTURHOEGSKOLEN("72"),
+    AULARKJELLEREN("282"),
+    FORSKNINGSVEIEN("283"),
+    FREDERIKKE("122"),
+    HANNAS("299"),
+    HELAA("295"),
+    IDRETTHOEGSKOLEN("157"),
+    INFORMATEKET("158"),
+    INFORMATIKK("284"),
+    OLE("154"),
+    AARSITDEN("296"),
+    KUTT("310"),
+    MEDICA("297"),
+    MUSIKKHOEGSKOLEN("159"),
+    ODONTOLOGI("298"),
+    SEILDUKEN("155"),
+    P35("71"),
+    SV("153"),
+    VETERINAERHOEGSKOLEN("160");
+    
+    private String value;
+    
+    CafeteriaEndpoint(String value) {
+    	this.value = value;
+    }
+    
+    public String value() {
+    	return this.value;
+    }
+   
+}
--- src/main/java/no/applitude/dagensbackend/Main.java
+++ src/main/java/no/applitude/dagensbackend/Main.java
@@ -2,10 +2,11 @@ package no.applitude.dagensbackend;
 
 import com.google.gson.Gson;
 
-class Main{
+class Main {
     public static void main(String []args){
         Gson g = new Gson();
         String []allergies = {"milk", "tomato"};
         System.out.println(g.toJson(new Dish("pizza", "nice pizza", allergies, "55kr", true)));
+        new TodaysDinnerRequestHandler();
     }
 }
