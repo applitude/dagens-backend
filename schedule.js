e49263cb87f68f85d1a43abd351bab4a3e6e0179
Roy Olav Purser <admin@royolav.net>
2016-03-13 16:17:25 +0100

    aws cloudwatch summertime scheduler

---
+++ schedule.js
@@ -0,0 +1,80 @@
+var AWS = require('aws-sdk');
+
+// simple script to schedule summertime on aws cloudwatch.
+
+var cloudwatchevents = new AWS.CloudWatchEvents();
+var context;
+const LENGTH_OF_DAY = 86400000;
+
+var run = 0;
+
+const SUMMER_PARAM = {
+    Name: 'todaysDinnerCronMondaySummer'
+};
+const WINTER_PARAM = {
+    Name: 'todaysDinnerCronMondayWinter'
+};
+
+exports.handler = function(event, c){
+    context = c;
+    
+    
+    var date = new Date();
+    
+    var month = date.getMonth();
+    var newMonth = month;
+    var sundays = 0;
+    
+    while(newMonth == month){
+        
+        // sunday is 0
+        
+        if(date.getDay() === 0){
+            sundays++;
+        }
+    
+        date.setTime(date.getTime() + LENGTH_OF_DAY); // goes to next day
+        
+        newMonth = date.getMonth();
+    }
+    
+    // if this sunday is the last of the month, or last sunday has passed
+    if(sundays <= 1){
+        if(month == 3){
+            enableSummer();
+        }else if(month == 10){
+            enableWinter();
+        }
+    }else{
+        if(month >= 1 && month <= 3 || month >= 10){
+            enableWinter();
+        }else if(month >= 3 && month <= 10){
+            enableSummer();
+        }
+    }
+}
+
+
+
+
+function enableSummer(){
+    cloudwatchevents.enableRule(SUMMER_PARAM, log);
+    cloudwatchevents.disableRule(WINTER_PARAM, log);
+}
+function enableWinter(){
+    cloudwatchevents.enableRule(WINTER_PARAM, log);
+    cloudwatchevents.disableRule(SUMMER_PARAM, log);
+}
+
+function log(err, data){
+    run++;
+    if(run < 2){
+        return;
+    }
+    
+    if (err) {
+        context.fail(err);
+    }else{
+        context.succeed(data);
+    }
+}
